3a71fe91ff33fea1e50f337f972302d2f70eac23
Priyam Carto <cpriyam90@gmail.com>
2022-05-03 15:59:27 -0400

    html added

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,34 @@
 const fs = require ("fs")
+function generateHTML (employees) {
+    var employeeData = ""
+    for (var i = 0; i < employees.length; i ++) {
+        if (employees[i].getRole() === "Manager") {
+            employeeData = employeeData + addManager (employees[i])
+        } 
+        else if (employees[i].getRole () === "Engineer") {
+            employeeData = employeeData + addEngineer (employees[i])
+        }
+        else if (employees[i].getRole () === "Intern") {
+            employeeData = employeeData + addIntern (employees[i])
+        }
+    }
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <title>Document</title>
+    </head>
+    <body>
+    ${employeeData}
+        
+    </body>
+    </html>
+    `
+}
 
 function addManager(manager) {
     return `
@@ -13,6 +43,7 @@ function addManager(manager) {
     </div>
     `
 }
+
 function addEngineer(engineer) {
     return `
     <div class="card" style="width: 18rem;">
@@ -26,7 +57,6 @@ function addEngineer(engineer) {
     </div>
     `
 }
-
 
 function addIntern(intern) {
     return `
