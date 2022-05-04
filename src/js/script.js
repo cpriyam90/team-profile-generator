9670adf12c2957e08499d9d1d1c907688f03a22c
Priyam Carto <cpriyam90@gmail.com>
2022-05-04 15:57:25 -0400

    extra typo

--- src/js/script.js
+++ src/js/script.js
@@ -46,7 +46,7 @@ function addEngineer(engineer) {
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${engineer.id}</li>
                 <li class="list-group-item">Github: <a href="https://github.com/${engineer.github}" target="_blank" class="card-link">${engineer.github}</a></li>
-                <li class="list-group-item">Email: <a href=""mailto:${engineer.email}" class="card-link">${engineer.email}</a></li>
+                <li class="list-group-item">Email: <a href="mailto:${engineer.email}" class="card-link">${engineer.email}</a></li>
             </ul>
         </div>
     </div>
@@ -64,7 +64,7 @@ function addIntern(intern) {
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${intern.id}</li>
                 <li class="list-group-item">School: ${intern.school}</li>
-                <li class="list-group-item">Email: <a href=""mailto:${intern.email}" class="card-link">${intern.email}</a></li>
+                <li class="list-group-item">Email: <a href="mailto:${intern.email}" class="card-link">${intern.email}</a></li>
             </ul>
         </div>
     </div>
